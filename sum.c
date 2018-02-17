0c52797b47bbfe570b2e2458042f0e6c50c8a0f2
My Name <My Email>
2018-02-17 22:34:18 +0800

    add new line

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,sum;		//定义变量a,b,sum
+	printf("please input two numbers a and b:");	
+	scanf("%d %d",&a,&b);		//输入整数到a,b
+	sum=a+b;		//对整数求和，结果存入sum
+	printf("sum=%d\n",sum);		//输出结果
+}
+//new line
